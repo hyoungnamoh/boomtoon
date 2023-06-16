b1ad4a79c3f9bc21d8f84061e81f4e00f043407d
hyoungnam <zzzsh789@naver.com>
2023-06-16 17:54:42 +0900

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Noto_Sans_Thai, Roboto } from 'next/font/google';
+
+const notosans = Noto_Sans_Thai({ subsets: ['latin'], weight: ['100', '300', '400', '500', '700', '900'] });
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={notosans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
